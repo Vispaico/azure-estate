ee9971fb60b095491aa8eaa27c7885a524d3cfec
Vispaico <inc@vispaico.com>
2025-07-14 21:06:12 +0700

    Clean up unused files and components

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,18 +1,9 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
- */
-
-/**
- * @type {import('gatsby').GatsbyConfig}
- */
+// gatsby-config.js
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `Azure Estates`,
+    description: `A showcase of luxury properties and high-end real estate.`,
+    author: `@vispaico`,
   },
   plugins: [
     `gatsby-plugin-image`,
@@ -25,19 +16,7 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
+    /* The gatsby-plugin-manifest has been removed to prevent icon errors.
+       We can add it back later with a custom site icon. */
   ],
-}
+};
